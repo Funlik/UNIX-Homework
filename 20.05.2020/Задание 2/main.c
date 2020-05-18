4e55ce78a4fe68353d3c1fef7b688d545255c162
kolyanok <nsokunkov@edu.hse.ru>
2020-05-18 16:37:15 +0300

    Форматирование

--- 20.05.2020/Задание 2/main.c
+++ 20.05.2020/Задание 2/main.c
@@ -4,11 +4,11 @@
 
 int main(int argc, const char * argv[])
 {
-		printf("Hello, World!\n");
+    printf("Hello, World!\n");
     printf("Running on \n");
-		#if __APPLE__
-			#include <TargetConditionals.h>
-		#endif
+    #if __APPLE__
+	#include <TargetConditionals.h>
+    #endif
     #if __APPLE__ && (TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE)
     	printf ("iOS");
     #else
@@ -24,7 +24,7 @@ int main(int argc, const char * argv[])
 	    }
 	    else
 	        printf("Unknown OS");
-	  #endif
+    #endif
     printf("\n");
     return 0;
 }
